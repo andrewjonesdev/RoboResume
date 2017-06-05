1c68dd6b517a97f05a432cbd3a7d6aa82911cfd2
Andrew D. Jones <andrewdjones@Andrews-MacBook-Pro-2.local>
2017-06-04 20:07:06 -0400

    Made it break proof, next steps compare months to see when start is after end date and add a way to sort by year

--- src/haveYouSeenRoboCup/RoboResume.java
+++ src/haveYouSeenRoboCup/RoboResume.java
@@ -11,32 +11,60 @@ public class RoboResume {
 		boolean againExt = false;
 		boolean againInt = false;
 		boolean againDuty = false;
+		boolean againGradYear = false;
 		boolean keepLooping = false;
 		String yesOrNo = "";
 		Scanner strScan = new Scanner(System.in);
 		Scanner numScan = new Scanner(System.in);
-		String name = "";
+		String nameInput = "";
 		String email = "";
 		ArrayList <String> resumeBullets = new ArrayList(); 
 		String selection = "";
 		String education = "";
-		String work = "";
-		String skill = "";
-		String rating = "";
+		//String work = "";
+		//String skill = "";
+		//String rating = "";
 		int countBullets = 0;
 		Education eduC = new Education();
 		Work workC = new Work();
 		Skill skillC = new Skill();
+		ArrayList <String> name = new ArrayList();
+		ArrayList <String> degree = new ArrayList();
+		ArrayList <String> school = new ArrayList();
+		ArrayList <String> major = new ArrayList();
+		ArrayList <String> jobTitle = new ArrayList();
+		ArrayList <String> employer = new ArrayList();
+		ArrayList <String> duty = new ArrayList();
+		ArrayList <String> skill = new ArrayList();
+		ArrayList <String> rating = new ArrayList();
+		int startYear = 0;
+		int endYear = 0;
 		
 		System.out.println("Welcome to Robo Resume)");
 		do{
-			
+			countBullets = 0;
 			do{
-				countBullets = 0;
 				System.out.println("Please enter your name)");
-				name = strScan.nextLine();
-				if(!name.isEmpty()){
+				String nameCheck = strScan.nextLine();
+				if(!nameCheck.isEmpty()){
 					keepLooping = false;
+					try{
+						name = capitalizeSentence(nameCheck);	
+					}catch (IndexOutOfBoundsException e)
+					{
+						System.out.println("Your name was not recognized. Please try again)");
+						keepLooping = true;
+						continue;
+					}
+					
+					//String nameInput = "";
+					nameInput = name.get(0);
+					if(name.size()>1){
+						for(int count = 1; count < name.size(); count ++){
+							nameInput += name.get(count);
+						}
+					}
+				
 				}
 				else{
 					System.out.println("Your name was not recognized. Please try again)");
@@ -46,7 +74,7 @@ public class RoboResume {
 			do{
 				System.out.println("Please enter your email)");
 				email = strScan.nextLine();
-				if (!email.isEmpty()&&(email.contains("@")&&(email.contains(".com")||email.contains(".gov")||email.contains(".org")||email.contains(".net")))){
+				if (!email.isEmpty()&&email.length()>=7&&(email.contains("@")&&(email.contains(".com")||email.contains(".gov")||email.contains(".org")||email.contains(".net")))){
 				
 					keepLooping = false;
 				}
@@ -66,37 +94,199 @@ public class RoboResume {
 					if(selection.equals("1")){
 							//keepLooping = false;
 							System.out.println("Enter an Educational Achievement)");
-							System.out.println("What is the degree you have obtained?)");
-							eduC.setHighestDegree("1" + strScan.nextLine());	
-							System.out.println("What School did you attend?)");
-							eduC.setSchool(strScan.nextLine());
-							System.out.println("What was your major?)");
-							eduC.setCourseOfStudy(strScan.nextLine());
+							do{
+								System.out.println("What is the degree you have obtained?)");
+								try{
+											degree = capitalizeSentence(strScan.nextLine());	
+								}catch (IndexOutOfBoundsException e)
+								{
+									System.out.println("Your degree was not recognized. Please try again)");
+									keepLooping = true;
+									continue;
+								}
+								
+								String degreeInput = "";
+								degreeInput = degree.get(0);
+								if(degree.size()>1){
+									for(int count = 1; count < degree.size(); count ++){
+										degreeInput += degree.get(count);
+									}
+								}
+								eduC.setHighestDegree("1" + degreeInput);
+								keepLooping=false;
+							}while(keepLooping);
+							do{
+								System.out.println("What School did you attend?)");
+								try{
+									school = capitalizeSentence(strScan.nextLine());	
+								}catch (IndexOutOfBoundsException e)
+								{
+									System.out.println("Your school was not recognized. Please try again)");
+									keepLooping = true;
+									continue;
+								}
+										String schoolInput = "";
+								schoolInput = school.get(0);
+								if(school.size()>1){
+									for(int count = 1; count < school.size(); count ++){
+										schoolInput += school.get(count);
+									}
+								}
+							keepLooping=false;
+							eduC.setSchool(schoolInput);
+							}
+							while(keepLooping);
+							do{
+								System.out.println("What was your major?)");
+								try{
+									major = capitalizeSentence(strScan.nextLine());	
+								}catch (IndexOutOfBoundsException e)
+								{
+									System.out.println("Your major was not recognized. Please try again)");
+									keepLooping = true;
+									continue;
+								}
+								
+								String majorInput = "";
+								majorInput = major.get(0);
+								if(major.size()>1){
+									for(int count = 1; count < major.size(); count ++){
+										majorInput += major.get(count);
+									}
+								}
+								keepLooping = false;
+								eduC.setCourseOfStudy(majorInput);
+							}
+							while(keepLooping);
+							int gradYear = 0;
+							do{
 							System.out.println("What year did you graduate?)");
-							eduC.setGradYear(numScan.nextInt());
+							//checks if letters are input
+								if (numScan.hasNextInt()){
+									gradYear = numScan.nextInt();
+									keepLooping = false;
+									//checks if the user has given a date that hasn't occured yet
+		
+									//checks if date is real or not. B.C. is not accepted
+									if(gradYear<0 || (Integer.toString(gradYear )).length()!=4){
+										System.out.println("The year you entered wasn't valid. Please try again.");	
+										keepLooping = true;
+									}
+
+								}else{
+									System.out.println("You didn't submit a whole number. Please try again.");
+									numScan.next();
+									keepLooping = true;
+									continue;
+								}
+							}while(keepLooping);
+							eduC.setGradYear(gradYear);
 							resumeBullets.add(eduC.toString());
 						}
 					else if(selection.equals("2")){
 						//keepLooping = false;
 						System.out.println("Enter a Work Experience)");
-						System.out.println("What is/was your job title during this experience?)");
-						workC.setJobTitle("2"+strScan.nextLine());
+						do{
+							System.out.println("What is/was your job title during this experience?)");
+							try{
+								jobTitle = capitalizeSentence(strScan.nextLine());	
+							}catch (IndexOutOfBoundsException e)
+							{
+								System.out.println("Your job title was not recognized. Please try again)");
+								keepLooping = true;
+								continue;
+							}
+									String jobTitleInput = "";
+							jobTitleInput = jobTitle.get(0);
+							if(jobTitle.size()>1){
+								for(int count = 1; count < jobTitle.size(); count ++){
+									jobTitleInput += jobTitle.get(count);
+								}
+							}
+						keepLooping=false;
+						workC.setJobTitle("2"+jobTitleInput);
+						}while(keepLooping);
 						//System.out.print(workC.getJobTitle());
-						System.out.println("Who is/was your employer?)");
-						workC.setEmployer(strScan.nextLine());	
-						System.out.println("What month and year did you start [ex. June 2016]?)");
-						String [] monthYear = (strScan.nextLine()).split(" ");
-						workC.setStartMonth(monthYear[0]);
-						workC.setStartYear((Integer.parseInt(monthYear[1])));
-						System.out.println("What month and year did you end [ex. June 2016]?)");
-						monthYear = (strScan.nextLine()).split(" ");
-						workC.setEndMonth(monthYear[0]);
-						workC.setEndYear((Integer.parseInt(monthYear[1])));
+						do{
+							System.out.println("Who is/was your employer?)");
+							try{
+								employer = capitalizeSentence(strScan.nextLine());	
+							}catch (IndexOutOfBoundsException e)
+							{
+								System.out.println("Your employer was not recognized. Please try again)");
+								keepLooping = true;
+								continue;
+							}
+									String employerInput = "";
+							employerInput = employer.get(0);
+							if(employer.size()>1){
+								for(int count = 1; count < employer.size(); count ++){
+									employerInput += employer.get(count);
+								}
+							}
+						keepLooping=false;
+						workC.setEmployer(employerInput);
+						}while(keepLooping);	
+						do{
+							try{
+								System.out.println("What month and year did you start [ex. June 2016]?)");
+								String [] monthYear = (strScan.nextLine()).split(" ");
+								workC.setStartMonth(capitalizeWord(monthYear[0]));
+								startYear = Integer.parseInt(monthYear[1]);
+								if(monthYear[1].length()!=4){
+									System.out.println("Invalid Year)");
+									keepLooping = true;
+									continue;
+								}
+								workC.setStartYear((Integer.parseInt(monthYear[1])));
+								System.out.println("What month and year did you end [ex. June 2016]?)");
+								monthYear = (strScan.nextLine()).split(" ");
+								workC.setEndMonth(capitalizeWord(monthYear[0]));
+								endYear = Integer.parseInt(monthYear[1]);
+								if(monthYear[1].length()!=4){
+									System.out.println("Invalid Year)");
+									keepLooping = true;
+									continue;
+								}
+								workC.setEndYear((Integer.parseInt(monthYear[1])));
+							}catch(IndexOutOfBoundsException | NumberFormatException e){
+								System.out.println("Month and Year not recognized. Please try again)");
+								keepLooping = true;
+								continue;
+							}
+							if(startYear>endYear){
+								System.out.println("End year should be greater than start year. Please try again)");
+								keepLooping = true;
+								continue;
+							}
+							else{
+								keepLooping = false;
+							}
+						}
+						while(keepLooping);
 						System.out.println("What were your duties?)");
 						workC.clearDuties();
-						do{
-						System.out.println("Enter a duty)");
-						workC.addDuty(strScan.nextLine());
+						do{	
+							do{
+								System.out.println("Enter a duty)");
+								try{
+									duty = capitalizeSentence(strScan.nextLine());	
+								}catch (IndexOutOfBoundsException e)
+								{
+									System.out.println("Your duty was not recognized. Please try again)");
+									keepLooping = true;
+									continue;
+								}
+										String dutyInput = "";
+								dutyInput = duty.get(0);
+								if(duty.size()>1){
+									for(int count = 1; count < duty.size(); count ++){
+										dutyInput += duty.get(count);
+									}
+								}
+							keepLooping=false;
+							workC.addDuty(dutyInput);
+						}while(keepLooping);
 						System.out.print("You can enter another duty. ");
 						}while(letsGoAgain(yesOrNo, againDuty, strScan));
 						resumeBullets.add(workC.toString());
@@ -104,22 +294,62 @@ public class RoboResume {
 					else{
 						//keepLooping = false;
 						System.out.println("Enter a Skill)");
-						System.out.println("Enter one of your skills)");
-						skillC.setSkillName("3"+strScan.nextLine());	
-						System.out.println("Enter a Rating for this Skill)");
-						skillC.setRating(strScan.nextLine());
+						
+						do{
+							System.out.println("Enter one of your skills)");
+							try{
+								skill = capitalizeSentence(strScan.nextLine());	
+							}catch (IndexOutOfBoundsException e)
+							{
+								System.out.println("Your skill was not recognized. Please try again)");
+								keepLooping = true;
+								continue;
+							}
+									String skillInput = "";
+							skillInput = skill.get(0);
+							if(skill.size()>1){
+								for(int count = 1; count < skill.size(); count ++){
+									skillInput += skill.get(count);
+								}
+							}
+						keepLooping=false;
+						skillC.setSkillName("3"+skillInput);
+						}while(keepLooping);	
+						do{
+							System.out.println("Enter a Rating for this Skill)");
+							try{
+								rating = capitalizeSentence(strScan.nextLine());	
+							}catch (IndexOutOfBoundsException e)
+							{
+								System.out.println("Your rating was not recognized. Please try again)");
+								keepLooping = true;
+								continue;
+							}
+									String ratingInput = "";
+							ratingInput = rating.get(0);
+							if(rating.size()>1){
+								for(int count = 1; count < rating.size(); count ++){
+									ratingInput += rating.get(count);
+								}
+							}
+						keepLooping=false;
+						skillC.setRating(ratingInput);
+						}while(keepLooping);
 						//skill = "2"+skill+", "+rating;
 						resumeBullets.add(skillC.toString());
 					}
 				}
 				else{
 					System.out.println("Invalid response. Please try again.");
-					keepLooping = true;
+					//System.out.println("Select 1 to enter an Educational Achievement)\nSelect 2 to enter a Work Experience)\nSelect 3 to enter a Skill)");
+					keepLooping = false;
+					//numScan.next();
+					continue;
 				}
 				}while(keepLooping);
 				System.out.print("You can enter another achievement. ");
 			}while(letsGoAgain(yesOrNo, againInt, strScan));
-			System.out.println(resumeGenerator(name, email,listSort(resumeBullets, '1', '2', '3')));
+			System.out.println(resumeGenerator(nameInput, email,listSort(resumeBullets, '1', '2', '3')));
 			//Where the resume print goes
 			System.out.print("You can enter another resume. ");
 		}while(letsGoAgain(yesOrNo, againExt, strScan));
@@ -198,6 +428,28 @@ public class RoboResume {
 		}while((!(yesOrNo.equals("Y"))) && (!(yesOrNo.equals("N"))));
 		
 		return again;
+	}
+	
+	public static ArrayList <String> capitalizeSentence(String split){
+		String [] postSplit = split.split(" ");
+		ArrayList <String> output = new ArrayList();
+		if(postSplit.length>1){
+			for(int count = 0; count < postSplit.length; count++){
+				if(count==postSplit.length-1){
+					output.add(capitalizeWord(postSplit[count]));
+				}
+				else{
+					output.add(capitalizeWord(postSplit[count]) + " ");
+				}
+			}
+		}
+		else{
+			output.add(capitalizeWord(postSplit[0]));
+		}
+		return output;
+	}
+	public static String capitalizeWord(String word){
+		return ((word.substring(0, 1)).toUpperCase() + (word.substring(1)).toLowerCase());
 	}
 	
 }
